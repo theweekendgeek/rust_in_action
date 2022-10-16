cbddd7e8c8c33a15256aeead254494c28e83ed71
Marcus Doescher <deh.marcus@outlook.de>
2022-10-16 19:53:28 +0200

    feat: more numbers

--- chapter_2/src/main.rs
+++ chapter_2/src/main.rs
@@ -4,7 +4,28 @@ fn main() {
     let c = 30i32;
     let d = 30_i32;
     let e = add(add(a, b), add(c, d));
-    println!("a + b + c + d = {}", e)
+    println!("a + b + c + d = {}", e);
+
+    let twenty = 20;
+    let twenty_one: i32 = 21;
+    let twenty_two = 22i32;
+
+    let addition = twenty + twenty_one + twenty_two;
+
+    println!("{} + {} + {} = {}", twenty, twenty_one, twenty_two, addition);
+
+    let one_million: i64 = 1_000_000;
+
+    println!("{}", one_million.pow(2));
+
+    let forty_twos = [
+        42.0,
+        42f32,
+        42.0_f32,
+    ];
+
+    println!("{:2}", forty_twos[0])
+
 }
 
 fn add(a: i32, b: i32) -> i32 {
