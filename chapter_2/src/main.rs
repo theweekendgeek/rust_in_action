1dfc31cfddc461f66e51efee2a535cfd3be99159
Marcus Doescher <deh.marcus@outlook.de>
2022-10-16 20:16:56 +0200

    feat: base x numbers

--- chapter_2/src/main.rs
+++ chapter_2/src/main.rs
@@ -4,30 +4,45 @@ fn main() {
     let c = 30i32;
     let d = 30_i32;
     let e = add(add(a, b), add(c, d));
+    println!("====================================");
     println!("a + b + c + d = {}", e);
 
+    // 2.3.1 Integers and decimal numbers
     let twenty = 20;
     let twenty_one: i32 = 21;
     let twenty_two = 22i32;
 
     let addition = twenty + twenty_one + twenty_two;
 
-    println!("{} + {} + {} = {}", twenty, twenty_one, twenty_two, addition);
+    println!("====================================");
+    println!(
+        "{} + {} + {} = {}",
+        twenty, twenty_one, twenty_two, addition
+    );
 
     let one_million: i64 = 1_000_000;
 
+    println!("====================================");
     println!("{}", one_million.pow(2));
 
-    let forty_twos = [
-        42.0,
-        42f32,
-        42.0_f32,
-    ];
+    let forty_twos = [42.0, 42f32, 42.0_f32];
 
-    println!("{:2}", forty_twos[0])
+    println!("====================================");
 
+    println!("{:2}", forty_twos[0]);
+
+    // 2.3.2 Integers with base 2, base 8, base 16
+
+    let three = 0b11;
+    let thirty = 0o36;
+    let three_hundred = 0x12C;
+
+    println!("base 10: {} {} {} ", three, thirty, three_hundred);
+    println!("base 2: {:b} {:b} {:b} ", three, thirty, three_hundred);
+    println!("base 8: {:o} {:o} {:o} ", three, thirty, three_hundred);
+    println!("base 16: {:x} {:x} {:x} ", three, thirty, three_hundred);
 }
 
 fn add(a: i32, b: i32) -> i32 {
-    a+b
+    a + b
 }
